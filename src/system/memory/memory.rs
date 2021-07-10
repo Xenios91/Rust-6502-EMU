ae3ac6ea495b8e7d77eca4e27bfcc82a130e5cc2
xenios <xenios91@protonmail.com>
2021-07-09 22:55:28 -0500

    updated the memory size to meet 16 bit max

--- src/system/memory/memory.rs
+++ src/system/memory/memory.rs
@@ -1,26 +1,26 @@
 pub struct Memory {
-    max_memory: u32,
-    data: [u8; 1024 * 64],
+    max_memory: u16,
+    data: [u8; 65535],
 }
 
 impl Memory {
     pub fn new() -> Self {
         Memory {
-            max_memory: 1024 * 64,
-            data: [0; 1024 * 64],
+            max_memory: 65535,
+            data: [0; 65535],
         }
     }
 
     fn initialize(&mut self) {
-        self.max_memory = 1024 * 64;
-        self.data = [0; 1024 * 64];
+        self.max_memory = 65535;
+        self.data = [0; 65535];
     }
 
     pub fn reset(&mut self) {
         self.initialize();
     }
 
-    pub fn get_total_memory(&self) -> u32 {
+    pub fn get_total_memory(&self) -> u16 {
         self.max_memory
     }
 
